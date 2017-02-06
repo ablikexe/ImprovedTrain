c98f758e273e1ec24de3f4b94cad13f49134745b
albert <ablikexe@gmail.com>
2017-02-06 18:53:53 +0100

    work on the appearance

--- app/src/main/java/ga/questionmark/improvedtrain/MainActivity.java
+++ app/src/main/java/ga/questionmark/improvedtrain/MainActivity.java
@@ -1,11 +1,20 @@
 package ga.questionmark.improvedtrain;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -32,6 +41,30 @@ public class MainActivity extends AppCompatActivity {
         "..xx.."
     };
     boolean c[] = new boolean[W*H+1];
+    CountDownTimer timer = null;
+
+    void playAgainOrExit(String msg) {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle(msg)
+                .setPositiveButton("Play again", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,25 +73,25 @@ public class MainActivity extends AppCompatActivity {
         current = 0;
         TableLayout ll = (TableLayout) findViewById(R.id.table);
 
-        CountDownTimer timer = new CountDownTimer(60000, 100) {
+        timer = new CountDownTimer(60000, 10) {
             @Override
             public void onTick(long millisUntilFinished) {
-                ((TextView)findViewById(R.id.timer)).setText(String.valueOf(millisUntilFinished) );
+                ((TextView)findViewById(R.id.timer)).setText(String.valueOf(millisUntilFinished/1000) + ":" + String.format("%02d", (millisUntilFinished%1000)/10));
             }
 
             @Override
             public void onFinish() {
-                c[99999] = 60 / (current-current) > 0;
+                playAgainOrExit("Time's up!");
 
             }
         };
         timer.start();
-        int numbers[][] = Rand.makeMatrix(W,H);
+        int numbers[][] = Rand.makeMatrix(H, W);
 
 
         int k = 0;
-        for (int i = 0; i < W; ++i) {
-            for (int j = 0; j < H; ++j) {
+        for (int i = 0; i < H; ++i) {
+            for (int j = 0; j < W; ++j) {
                 sorted[k] = numbers[i][j];
                 ++k;
             }
@@ -66,20 +99,51 @@ public class MainActivity extends AppCompatActivity {
 
         Arrays.sort(sorted);
 
-        for (int i = 0; i < W; i++)
-            for (int j = 0; j < H; j++)
+        for (int i = 0; i < H; i++)
+            for (int j = 0; j < W; j++)
                 c[numbers[i][j]] = (pattern[i].charAt(j) == 'x');
 
-        for (int i = 0; i <W; i++) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-            TableRow row= new TableRow(this);
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
 
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        int cell_size = Math.min((int)(0.8*width)/W, (int)(0.8*height)/H)-2;
+        int table_width = (cell_size+2)*W + (int)(0.1 * Math.min(width, height));
+        int table_height = (cell_size+2)*H + (int)(0.1 * Math.min(width, height));
+        ll.setMinimumHeight(table_height);
+
+        for (int i = 0; i < H; i++) {
+
+            TableRow row = new TableRow(this);
+
+            TableRow.LayoutParams lp = new TableRow.LayoutParams();
+            lp.height = cell_size;
+            lp.width = table_width;
+            // lp.topMargin = 10;
+            // lp.bottomMargin = 10;
             row.setLayoutParams(lp);
-            for (int j = 0; j < H; j++) {
+            // row.setBackgroundColor(0x0ff194759);
+            // row.setPadding(0, 1, 0, 1);
+            row.setGravity(Gravity.CENTER);
+
+            for (int j = 0; j < W; j++) {
                 TextView tv = new TextView(this);
-                tv.setTextSize(50);
-                tv.setWidth(125);
+                tv.setTextSize(35);
+                tv.setTextColor(0xffd8f2f0);
+                tv.setWidth(cell_size);
+                tv.setHeight(cell_size);
+                tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                tv.setGravity(Gravity.CENTER);
+                tv.setBackgroundColor(0xff296b73);
+                TableRow.LayoutParams lp2 = new TableRow.LayoutParams();
+                lp2.rightMargin = 1;
+                lp2.leftMargin = 1;
+                lp2.topMargin = 1;
+                lp2.bottomMargin = 1;
+                tv.setLayoutParams(lp2);
+                // tv.setPadding(10, 10, 10, 10);
                 tv.setText(String.valueOf(numbers[i][j]));
                 tv.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -88,20 +152,23 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("", number + " " + String.valueOf(sorted[current]));
                         if (number.equals("") || number.equals(":)")) {
                             // ((TextView) v).setText(":)");
-                        } else
-                        if (Integer.valueOf(number) == sorted[current]) {
-                            ((TextView) v).setText(c[Integer.valueOf(number)] ? ":)" : "");
-                            current++;
-                        } else {
+                        } else if (number.equals(":(") || Integer.valueOf(number) != sorted[current]) {
                             ((TextView) v).setText(":(");
+                            playAgainOrExit("You lost!");
+                        } else {
+                            // ((TextView) v).setText(c[Integer.valueOf(number)] ? ":)" : "");
+                            ((TextView) v).setText("");
+                            ((TextView) v).setBackgroundColor(0x0ff194759);
+                            // ((TextView) v).setTextSize(15);
+                            current++;
+                            if (current >= W*H) playAgainOrExit("You won!");
                         }
-
-
                     }
                 });
                 row.addView(tv);
             }
-            ll.addView(row,i);
+            ll.addView(row, i);
         }
+        ll.setPadding(0, 20, 0, 20);
     }
 }
